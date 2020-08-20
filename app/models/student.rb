9ed9757f1fd61ae7d30e74f19bdf42ca8c190a93
LetsJessCode <letsjesscode@gmail.com>
2020-08-20 16:29:30 -0400

    added devise

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,9 @@
 class Student < ApplicationRecord
-    has_many :assignments
-    has_many :subjects, through: :assignments
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :assignments
+  has_many :subjects, through: :assignments
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
